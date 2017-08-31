901454f30da8c96f6a31f5bab896934573018bce
Alex Snaps <alex.snaps@shopify.com>
2017-08-31 14:42:38 -0400

    Recursive tracking

--- ext/gctrack/gctrack.c
+++ ext/gctrack/gctrack.c
@@ -4,70 +4,93 @@
 #include <stdbool.h>
 #include <errno.h>
 #include <time.h>
+#include <stdlib.h>
 
 typedef struct {
   unsigned long cycles;
-  unsigned long duration;
+  unsigned long long duration;
   void *parent;
-} stat_record;
+} record_t;
 
-static VALUE mGC;
+static VALUE cTracker;
 static ID id_tracepoint;
 
 static bool enabled = false;
 
-static stat_record *last_record = NULL;
-static unsigned long last_enter;
+static record_t *last_record = NULL;
+static unsigned long long last_enter = 0;
 
-static unsigned long nanotime()
+static unsigned long long 
+nanotime()
 {
   struct timespec ts;
   if (clock_gettime(CLOCK_MONOTONIC, &ts) == -1) {
     rb_sys_fail("clock_gettime");
   }
-  return ts.tv_sec * 1e9 + ts.tv_nsec;
+  return ts.tv_sec * (unsigned long long) 1000000000 + ts.tv_nsec;
 }
 
-static void gc_hook(VALUE tpval, void *data)
+static inline void 
+add_gc_cycle(unsigned long long duration)
 {
-  if (!enabled || !last_record) {
-      return;
+  record_t *record = last_record;
+  while (record) {
+    record->cycles = record->cycles + 1;
+    record->duration = record->duration + duration;
+    record = (record_t *) record->parent;
+  }
+}
+
+static void 
+gctracker_hook(VALUE tpval, void *data)
+{
+  if (!enabled) {
+    return;
   }
   rb_trace_arg_t *tparg = rb_tracearg_from_tracepoint(tpval);
   switch (rb_tracearg_event_flag(tparg)) {
-      case RUBY_INTERNAL_EVENT_GC_ENTER:
-        last_enter = nanotime();
-        break;
-      case RUBY_INTERNAL_EVENT_GC_EXIT:
-        last_record->cycles++;
-        last_record->duration += nanotime() - last_enter;
-        break;
+    case RUBY_INTERNAL_EVENT_GC_ENTER: {
+      last_enter = nanotime();
+    }
+      break;
+    case RUBY_INTERNAL_EVENT_GC_EXIT: {
+      if (last_enter) {
+        add_gc_cycle(nanotime() - last_enter);
+      }
+      last_enter = 0;
+    }
+      break;
   }
 }
 
 static VALUE
-start_record(int argc, VALUE *argv, VALUE klass)
+gctracker_start_record(int argc, VALUE *argv, VALUE klass)
 {
-  stat_record *record = calloc(1, sizeof(start_record));
+  if(!enabled) {
+    return Qfalse;
+  }
+
+  record_t *record = (record_t *) calloc(1, sizeof(record_t));
   if (!record) {
     return Qfalse;
-  }    
+  }
+  
   record->parent = last_record;
   last_record = record;
   return Qtrue;
 }
 
 static VALUE
-end_record(int argc, VALUE *argv, VALUE klass)
+gctracker_end_record(int argc, VALUE *argv, VALUE klass)
 {
   if (last_record) {
-    stat_record *record = last_record;
-  
+    record_t *record = last_record;
+    last_record = (record_t *) record->parent;
+    
     VALUE stats = rb_ary_new2(2);
     rb_ary_store(stats, 0, ULONG2NUM(record->cycles));
     rb_ary_store(stats, 1, ULONG2NUM(record->duration));
   
-    last_record = record->parent;
     free(record);  
 
     return stats;
@@ -76,51 +99,49 @@ end_record(int argc, VALUE *argv, VALUE klass)
 }
 
 static VALUE
-enable(int argc, VALUE *argv, VALUE klass)
+gctracker_enable(int argc, VALUE *argv, VALUE klass)
 {
-  VALUE tracepoint = rb_ivar_get(mGC, id_tracepoint);
+  VALUE tracepoint = rb_ivar_get(cTracker, id_tracepoint);
   if (NIL_P(tracepoint)) {
+    enabled = true;
     rb_event_flag_t events;
-    events = RUBY_INTERNAL_EVENT_GC_START | RUBY_INTERNAL_EVENT_GC_END_MARK | RUBY_INTERNAL_EVENT_GC_END_SWEEP;
-    events |= RUBY_INTERNAL_EVENT_GC_ENTER | RUBY_INTERNAL_EVENT_GC_EXIT;
-    VALUE tracepoint = rb_tracepoint_new(0, events, gc_hook, (void *)0);
+    events = RUBY_INTERNAL_EVENT_GC_ENTER | RUBY_INTERNAL_EVENT_GC_EXIT;
+    tracepoint = rb_tracepoint_new(0, events, gctracker_hook, (void *) NULL);
     if (NIL_P(tracepoint)) { 
+      enabled = false;
       return Qfalse;
     }
+    rb_ivar_set(cTracker, id_tracepoint, tracepoint);
     rb_tracepoint_enable(tracepoint);
-    rb_ivar_set(mGC, id_tracepoint, tracepoint);
+  } else if (!enabled) {
     enabled = true;
-  } else {
-    if (!enabled) {
-      enabled = true;
-      rb_raise(rb_eTypeError, "GC::Tracker: CORRUPTED FSM!");
-    }
+    rb_raise(rb_eTypeError, "GCTracker: CORRUPTED FSM!");
   }
-
+  
   return Qtrue;
 }
 
 static VALUE
-disable(VALUE self)
+gctracker_disable(VALUE self)
 {
-  VALUE tracepoint = rb_ivar_get(mGC, id_tracepoint);  
+  VALUE tracepoint = rb_ivar_get(cTracker, id_tracepoint);  
   if (NIL_P(tracepoint)) {
     if (enabled) {
       enabled = false;
-      rb_raise(rb_eTypeError, "GC::Tracker: CORRUPTED FSM!");
+      rb_raise(rb_eTypeError, "GCTracker: CORRUPTED FSM!");
     }
     return Qfalse;
   }
 
+  rb_tracepoint_disable(tracepoint);
+  rb_ivar_set(cTracker, id_tracepoint, Qnil);
+  enabled = false;
   while (last_record) {
-    stat_record *record = last_record;
-    last_record = record->parent;
+    record_t *record = last_record;
+    last_record = (record_t *) record->parent;
     free(record);
   }
-
-  rb_tracepoint_disable(tracepoint);
-  rb_ivar_set(mGC, id_tracepoint, Qnil);
-  enabled = false;
+  last_record = NULL;
   
   return Qtrue;
 }
@@ -128,15 +149,15 @@ disable(VALUE self)
 void
 Init_gctrack()
 {
-  mGC = rb_define_module("GC");
-  VALUE cTracker = rb_define_class_under(mGC, "Tracker", rb_cData);
+  VALUE mGC = rb_define_module("GC");
+  cTracker = rb_define_module_under(mGC, "Tracker");
 
   id_tracepoint = rb_intern("__gc_tracepoint");  
-  rb_ivar_set(mGC, id_tracepoint, Qnil);
+  rb_ivar_set(cTracker, id_tracepoint, Qnil);
 
-  rb_define_singleton_method(cTracker, "enable", enable, 0);
-  rb_define_singleton_method(cTracker, "disable", disable, 0);
+  rb_define_module_function(cTracker, "enable", gctracker_enable, 0);
+  rb_define_module_function(cTracker, "disable", gctracker_disable, 0);
 
-  rb_define_singleton_method(cTracker, "start_record", start_record, 0);
-  rb_define_singleton_method(cTracker, "end_record", end_record, 0);
+  rb_define_module_function(cTracker, "start_record", gctracker_start_record, 0);
+  rb_define_module_function(cTracker, "end_record", gctracker_end_record, 0);
 }
--- test/test_gctrack.rb
+++ test/test_gctrack.rb
@@ -11,9 +11,7 @@ class TestGctrack < Test::Unit::TestCase
   def test_disable
     assert !GC::Tracker.disable
     assert GC::Tracker.enable
-    assert GC::Tracker.enable
     assert GC::Tracker.disable
-    assert !GC::Tracker.disable
   end
 
   def test_returns_false_when_not_enabled
@@ -27,6 +25,37 @@ class TestGctrack < Test::Unit::TestCase
     cycles, duration = GC::Tracker.end_record
     assert cycles > 0
     assert duration > 0
+  ensure
+    GC::Tracker.disable
+  end
+
+  def test_recurses
+    assert GC::Tracker.enable
+    assert GC::Tracker.start_record
+    a = "a"
+    10.times { |i| a += a * i }
+    GC.start
+    assert GC::Tracker.start_record
+    a = "a"
+    10.times { |i| a += a * i }
+    cycles_c, duration_c = GC::Tracker.end_record
+    cycles_p, duration_p = GC::Tracker.end_record
+    assert cycles_p > cycles_c
+    assert duration_p > duration_c
+  ensure
+    GC::Tracker.disable
+  end
+
+  def test_no_data_when_disabled
+    assert !GC::Tracker.start_record
+    assert GC::Tracker.end_record.nil?
+  end
+
+  def test_no_data_for_started_records_on_disable
+    assert GC::Tracker.enable
+    assert GC::Tracker.start_record
+    assert GC::Tracker.disable
+    assert GC::Tracker.end_record.nil?
   ensure
     GC::Tracker.disable
   end
